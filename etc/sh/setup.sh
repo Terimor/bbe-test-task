e902d9392458f6b72beeb41eb4b20b8f56803171
vladkyr <vlad.kyr199@gmail.com>
2022-04-01 04:59:12 +0300

    vendor commit

--- etc/sh/setup.sh
+++ etc/sh/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd /var/www/html && sudo docker-compose up -d --build
-sudo docker exec btt_php php composer.phar install
-[[ ! -d /var/www/html/config/jwt ]] && sudo docker exec btt_php bin/console lexik:jwt:generate-keypair && echo 'Generated keypair for Auth'
+#sudo docker exec btt_php php composer.phar install
+#[[ ! -d /var/www/html/config/jwt ]] && sudo docker exec btt_php bin/console lexik:jwt:generate-keypair && echo 'Generated keypair for Auth'
 sudo docker exec btt_php bin/console --no-interaction doctrine:migrations:migrate
